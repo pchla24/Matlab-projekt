0382a689349bdf74209f99d735cb209a9aa2a681
pchla24 <janusz_sawicki97@o2.pl>
2018-05-31 16:25:15 +0200

    okreslanie liczby klas

--- dane_ilosciowe.m
+++ dane_ilosciowe.m
@@ -1,5 +1,7 @@
 % Dane ilosciowe
 
+clear;
+
 load projekt16.mat
 data = t;
 data_cell = table2cell(t);
@@ -7,15 +9,24 @@ data_num = cell2mat(data_cell);
 
 all = height(data);
 Ilosc_wszystkich_obiektow = all
-klasa_1 = height(data(data.klasa==1,1));
-klasa_2 = height(data(data.klasa==2,1));
-klasa_3 = height(data(data.klasa==3,1));
-klasa_4 = height(data(data.klasa==4,1));
 
-dane_ilosciowe = [ klasa_1, klasa_2, klasa_3 klasa_4 ];
+class_sort = sort(data_num,12);
+class_n = class_sort(end,end);
+Ilosc_wszystkich_klas = class_n
+
+klasa_1 = height(data(data.klasa==1,1));
+Ilosc_obiektow_klasy_1 = klasa_1
+klasa_2 = height(data(data.klasa==2,1));
+Ilosc_obiektow_klasy_2 = klasa_2
+klasa_3 = height(data(data.klasa==3,1));
+Ilosc_obiektow_klasy_3 = klasa_3
+klasa_4 = height(data(data.klasa==4,1));
+Ilosc_obiektow_klasy_4 = klasa_4
+
+dane_ilosc = [ klasa_1, klasa_2, klasa_3 klasa_4 ];
 figure
-pie(dane_ilosciowe);
-title('Ilosc obiektow z podzialem na klasy')
+pie(dane_ilosc);
+title('Stosunek procentowy obiektow z podzialem na klasy')
 
 lgd = legend('Klasa 1','Klasa 2','Klasa 3','Klasa 4');
 lgd.Position = [ 0 0.1 0.3 0 ];
