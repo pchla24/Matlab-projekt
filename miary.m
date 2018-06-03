ab0cdef91a08d6da7d8d6eb5fd342cf5e54343c8
pchla24 <janusz_sawicki97@o2.pl>
2018-06-03 18:59:00 +0200

    Wykresy średnich

--- miary.m
+++ miary.m
@@ -12,21 +12,71 @@ klasa2 = data_num(data_num(:,12)==2,:);
 klasa3 = data_num(data_num(:,12)==3,:);
 klasa4 = data_num(data_num(:,12)==4,:);
 
-% Srednie ------------------------------
-Srednie = zeros(4,11);
+% Srednie ---------------------------------------------------
 
-for i=1:11
-    Srednie(1,i) = mean(klasa1(:,i));
+Srednie = zeros(4,11);
+for i=1:4
+    for j=1:11
+        Srednie(i,j) = mean(data_num(data_num(:,12)==i,j));
+    end
 end
-for i=1:11
-    Srednie(2,i) = mean(klasa2(:,i));
-end
-for i=1:11
-    Srednie(3,i) = mean(klasa3(:,i));
-end
-for i=1:11
-    Srednie(4,i) = mean(klasa4(:,i));
-end
+
+klasy = categorical({'Klasa 1','Klasa 2','Klasa 3','Klasa 4'});
+
+figure
+subplot(3,4,1)
+bar(klasy,Srednie(:,1));
+title('dat1');
+ylabel('Wartosc srednia')
+subplot(3,4,2)
+bar(klasy,Srednie(:,2));
+title('dat2');
+ylabel('Wartosc srednia')
+subplot(3,4,3)
+bar(klasy,Srednie(:,3));
+title('dat3');
+ylabel('Wartosc srednia')
+subplot(3,4,4)
+bar(klasy,Srednie(:,4));
+title('dat4');
+ylabel('Wartosc srednia')
+subplot(3,4,5)
+bar(klasy,Srednie(:,5));
+title('dat5');
+ylabel('Wartosc srednia')
+subplot(3,4,6)
+bar(klasy,Srednie(:,6));
+title('dat6');
+ylabel('Wartosc srednia')
+subplot(3,4,7)
+bar(klasy,Srednie(:,7));
+title('dat7');
+ylabel('Wartosc srednia')
+subplot(3,4,8)
+bar(klasy,Srednie(:,8));
+title('dat8');
+ylabel('Wartosc srednia')
+subplot(3,4,9)
+bar(klasy,Srednie(:,9));
+title('dat9');
+ylabel('Wartosc srednia')
+subplot(3,4,10)
+bar(klasy,Srednie(:,10));
+title('dat10');
+ylabel('Wartosc srednia')
+subplot(3,4,11)
+bar(klasy,Srednie(:,11));
+title('dat11');
+ylabel('Wartosc srednia')
+
+
+
+
+
+
+        
+        
+        
 
         
         
