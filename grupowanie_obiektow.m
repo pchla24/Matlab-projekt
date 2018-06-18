42f30300655c0bc5e12a377bb329436f66c13797
pchla24 <janusz_sawicki97@o2.pl>
2018-06-18 18:12:19 +0200

    grupowanie do poprawy

--- grupowanie_obiektow.m
+++ grupowanie_obiektow.m
@@ -16,9 +16,8 @@ data_do_grup.Properties.VariableNames = datX;
 % klasy = kmeans([data_do_grup.dat4 data_do_grup.dat5 data_do_grup.dat6 ...
 %     data_do_grup.dat8 data_do_grup.dat7],4,'Replicates',10);
 
-klasy = kmeans([data_do_grup{:,:}],4,'Replicates',10);
-
-test1 = crosstab(klasy,data.klasa);
+klasy = kmeans([data_do_grup.dat7 data_do_grup.dat8],4,'Replicates',10);
+test1 = crosstab(klasy,data.klasa)
 
 % c = clusterdata(data_do_grup,'maxclust',4,'linkage','complete');
 % test1 = crosstab(c,data.klasa);
