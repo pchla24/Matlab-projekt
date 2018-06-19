642974c2fc9ab44820c412c0c5a6eb13c5febbce
zychp <pawelzych97@gmail.com>
2018-06-19 23:30:41 +0200

    hotfix

--- grupowanie_obiektow.m
+++ grupowanie_obiektow.m
@@ -61,8 +61,8 @@ for i=1:11
     scatter(data_do_grup.dat7,data_do_grup.dat8,10,c)
     for j=1:i
         avg=zeros(1);
-        avg(j,1) = mean(data_do_grup{c==j,7});
-        avg(j,2) = mean(data_do_grup{c==j,8});
+        avg(1,1) = mean(data_do_grup{c==j,7});
+        avg(1,2) = mean(data_do_grup{c==j,8});
         hold on 
         scatter( avg(:,1),avg(:,2),10,[1 0 0],'*')
         hold off
